b531185255fb5d7e90a4681d6b09164086c26c6c
Blake Fealy <58127076+blakefealy@users.noreply.github.com>
2025-08-02 17:27:51 -0700

    Add files via upload

---
+++ CEP Extension Template/js/main.js
@@ -0,0 +1,31 @@
+(function () {
+ 'use strict';
+ var extPath;
+ extPath = location.href;
+	if(getOS() == "MAC") {
+		extPath = extPath.substring(7, extPath.length - 11);
+	}
+	if(getOS() == "WIN") {
+		extPath = extPath.substring(8, extPath.length - 11);
+	}
+
+	console.log("Start");
+	console.log(extPath);
+
+ }());
+
+//Preloaded Get OS function and function runs immediately above for placeholders
+function getOS() {
+ 		var userAgent = window.navigator.userAgent,
+ 		platform = window.navigator.platform,
+ 		macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+ 		windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+ 		os = null;
+
+ 		if(macosPlatforms.indexOf(platform) != -1) {
+ 			os = "MAC";
+ 		} else if(windowsPlatforms.indexOf(platform) != -1) {
+ 			os = "WIN";
+ 		}
+ 		return os;
+ 	}
